ed20027f928d66162e5be0314de34a83bed9dea6
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-07-05 13:54:51 -0700

    Add Comparable mixin to Node

--- binarysearchtree.rb
+++ binarysearchtree.rb
@@ -1,9 +1,14 @@
 class Node
-
   attr_accessor :value, :left_node, :right_node
 
-  def initialize
-    @value = nil
+  include Comparable
+
+  def <=>(other)
+    @value <=> other.value
+  end
+
+  def initialize (value = nil)
+    @value = value
     @left_node = nil
     @right_node = nil
   end
@@ -27,4 +32,3 @@ class Tree
 
 
 end
-puts a_node
